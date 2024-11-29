602454f755d86579950ca9e10bd138b0dffa02e8
Mendel Mendelovich <meni905@gmail.com>
2024-11-30 00:52:01 +0200

    fix wrong import

--- src/components/Layout/Navbar.tsx
+++ src/components/Layout/Navbar.tsx
@@ -4,7 +4,7 @@ import { useState } from "react";
 import Link from "next/link";
 import Image from "next/image";
 import { Menu, X } from "lucide-react";
-import logo from "../app/logo.png";
+import logo from "../../app/logo.png";
 import Logout from "../auth/Logout";
 import type { User } from "lucia";
 import {
